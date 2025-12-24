9571ca203c7a313d1fbd1ef87f198037d395e287
amandx36 <xamandeep360@gmail.com>
2025-12-24 23:28:13 +0530

    Entity  to database of otp is done

--- src/main/java/com/SecureAuth/SecureAuth/Api/model/OTP.java
+++ src/main/java/com/SecureAuth/SecureAuth/Api/model/OTP.java
@@ -1,4 +1,54 @@
 package com.SecureAuth.SecureAuth.Api.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "otps")
+@Data
 public class OTP {
+
+    // defining the enum type dude
+    public enum OTPType {
+        LOGIN,
+        REGISTER,
+        PASSWORD_RESET
+    }
+
+    // making the primary key dude
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "otp_code", length = 6)
+    private String otpCode;
+
+    @Enumerated(EnumType.STRING)
+    private OTPType type;
+
+    // data type for storing the expiry time
+
+    @Column(name = "expires_at")
+    private LocalDateTime expiresAt;
+
+    private boolean used = false;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    // function to check is the otp is expired dude !!
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(expiresAt);
+    }
+
+    // function to check is the otp is valid or not
+    public boolean isValid() {
+        return !used && !isExpired();
+    }
+
 }
