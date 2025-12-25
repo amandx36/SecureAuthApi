3de0e35f09a76416a30ec2fc9325fbd812f8498e
amandx36 <xamandeep360@gmail.com>
2025-12-25 20:59:41 +0530

    Added again otp request

--- src/main/java/com/SecureAuth/SecureAuth/Api/dto/OTPRequest.java
+++ src/main/java/com/SecureAuth/SecureAuth/Api/dto/OTPRequest.java
@@ -1,4 +1,18 @@
 package com.SecureAuth.SecureAuth.Api.dto;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class OTPRequest {
+    @NotBlank  @Email
+    private  String email ;
+
+    @NotBlank
+    @Size(min =  6 , max =  6 , message = "OTP size must be equal to 6 ")
+    private String otp ;
+
 }
